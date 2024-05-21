cf85ff1a048e92ce15873a3a6c8cb24626b98dc7
5100-chap <A01275100@tec.mx>
2024-05-20 19:05:40 -0600

    Implementacion c++-python

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # main.py
 import vt
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 import os
 import sys
 import model
 
 load_dotenv()
 
-url = str()
-if len(sys.argv)<2:  
+url = input("Introduzca un URL: ")
+if len(url) < 2:
     print("Por favor introduzca un valor")
-elif 'APIKEY' in os.environ:
-    url = sys.argv[1]
+elif "APIKEY" in os.environ:
     try:
-        client = vt.Client(str(os.getenv('APIKEY')))
-        
+        is_valid = model.is_valid_url(url)
+        client = vt.Client(str(os.getenv("APIKEY")))
+
         info = client.get_object("/urls/{}".format(vt.url_id(url)))
         print(info.last_analysis_stats)
 
-        analysis = client.scan_url(url)  
+        analysis = client.scan_url(url)
         print(analysis)
 
-        is_valid = model.is_valid_url(url)
-        print(f"La URL es {'válida' if is_valid else 'inválida'} según el validador de C++.")
+        print(
+            f"La URL es {'válida' if is_valid else 'inválida'} según el validador de C++."
+        )
 
         client.close()
     except NameError:

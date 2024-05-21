189fea449bd091f39cbf4ef58ad1c5b21e7ee2a5
5100-chap <A01275100@tec.mx>
2024-05-20 18:40:10 -0600

    added cpython implementation

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+# main.py
 import vt
-from dotenv import load_dotenv
+from dotenv import load_dotenv  
 import os
 import sys
+import model
 
 load_dotenv()
 
 url = str()
-if len(sys.argv)<2:
+if len(sys.argv)<2:  
     print("Por favor introduzca un valor")
-    # check if api key exists
 elif 'APIKEY' in os.environ:
     url = sys.argv[1]
-    # connection to API with a client
     try:
         client = vt.Client(str(os.getenv('APIKEY')))
         
-        # get information about an URL
         info = client.get_object("/urls/{}".format(vt.url_id(url)))
         print(info.last_analysis_stats)
 
-        analysis = client.scan_url(url)
+        analysis = client.scan_url(url)  
         print(analysis)
 
-        client.close() # always close session
+        is_valid = model.is_valid_url(url)
+        print(f"La URL es {'válida' if is_valid else 'inválida'} según el validador de C++.")
+
+        client.close()
     except NameError:
         print(NameError)
--- models/model.cpp
+++ models/model.cpp
@@ -1,28 +1,34 @@
+// Librerias
+#include <Python.h>
 #include <iostream>
 #include <string>
 #include "Tree.hpp"
-using namespace std;
 
-int main(int argc, char *argv[])
-{
-    if (argc > 1)
-    {
-        Tree *bin = new Tree();
-        string url(argv[1]);
-        bin->insert(url);
-        bin->print();
-
-        URLGrammar grammar;
-        if (grammar.isValidURL(url))
-        {
-            cout << "La URL es válida." << endl;
-        }
-        else
-        {
-            cout << "La URL es sus." << endl;
-        }
-
-        delete bin;
+static PyObject* is_valid_url(PyObject* self, PyObject* args) {
+    const char* url;
+    if (!PyArg_ParseTuple(args, "s", &url)) {
+        return NULL;
     }
-    return 0;
+    
+    URLGrammar grammar;
+    bool is_valid = grammar.isValidURL(std::string(url));
+    
+    return PyBool_FromLong(is_valid);
+}
+
+static PyMethodDef ModelMethods[] = {
+    {"is_valid_url", is_valid_url, METH_VARARGS, "Check if a URL is valid"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef modelmodule = {
+    PyModuleDef_HEAD_INIT,
+    "model",
+    "A module to validate URLs",
+    -1,
+    ModelMethods
+};
+
+PyMODINIT_FUNC PyInit_model(void) {
+    return PyModule_Create(&modelmodule);
 }
